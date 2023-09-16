d64bfb4b9a11857f8e1855f37e2bdd54894e2af6
flushpot1125 <crescentblue1122@gmail.com>
2023-09-16 16:47:06 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+//参考：https://qiita.com/10mi8o/items/2477f2640291f0ce6687
+//参考：https://qiita.com/chocomint_t/items/4bc57945bce081922582
+
+/* pathモジュールを使ってwebページとして出力するパスを指定する。  
+   "path.resolveは、引数をつなげて絶対パスに変換する"
+   "__dirname"は、実行集のソースコードが格納されているディレクトリパス
+   参考：https://gist.github.com/uupaa/da42698d6b2d2cbb3cca
+*/
+const path = require('path');
+//const outputPath = path.resolve(__dirname, 'dist');
+const outputPath = path.resolve(__dirname, './');//distではなくプロジェクトのトップに変更
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',//起点となるファイルのみを指定。src以下のファイルを個別に指定する必要はない
+    output: {
+        // バンドルしてmain.jsとして出力（これは実体として生成されないが、index.htmlなどで呼び出し記述が必要）
+        filename: 'main.js',
+        path: outputPath
+    },
+    // webpack-dev-serverを立ち上げた時のドキュメントルートを設定
+    // 
+    devServer: {
+        static:{
+            directory: outputPath,
+            watch:true//html,cssなどに変更があればブラウザリロードを自動実行
+        },
+        port: 8080
+    }
+}
